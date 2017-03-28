13cdcf3a87abebb43107d65b1fc0315c6590c54a
Bani14 <vbhavanareddy1@gmail.com>
2017-03-28 16:59:10 -0400

    Build method is added.

--- Jenkins/src/HelloJenkins.java
+++ Jenkins/src/HelloJenkins.java
@@ -3,8 +3,14 @@ public class HelloJenkins {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Build sucessful using GIT");
+		System.out.println("Add to github");
+		Build();
 
+	}
+
+	 static void Build() {
+		// TODO Auto-generated method stub
+		 System.out.println("Build sucessful using GIT");
 	}
 
 }
